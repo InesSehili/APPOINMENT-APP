7e5d66711458e7474e6667a4b3803ac57e8f59e3
InesSehili <inessehili96@gmail.com>
2023-12-03 21:49:08 +0100

    first commi

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Appointment } from '../models/appointment';
 
 @Component({
   selector: 'app-appointment-list',
@@ -6,7 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./appointment-list.component.css']
 })
 export class AppointmentListComponent {
+  title1: string = "";
+  date1 : Date =  new Date();
+  appointment : Appointment [] = [];
 
-  name:string = "INES"
+addAppointment() {
+  console.log( this.title1.trim.length + " " + this.date1)
+  if (this.title1.trim().length && this.date1) {
+    let newAppointment : Appointment = {
+      id : 1,
+      title : this.title1,
+      date : this.date1
+     }
+     this.appointment.push(newAppointment);
+     this.title1 = "";
+     this.date1 = new Date();
+
+
+     alert(this.appointment.length);
+
+  }
+
+
+
+}
+
+
+
 
 }
