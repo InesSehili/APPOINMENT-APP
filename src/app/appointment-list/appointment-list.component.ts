eb267922e2ed6763935d0e641c7aa8c5999129b2
INES SEHILI <inessehili96@gmail.com>
2023-12-06 12:22:01 +0100

    local storage + bootstrap styling

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
-
+import { OnInit } from '@angular/core';
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css']
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit {
+  ngOnInit(): void {
+    let saveappointment = localStorage.getItem("appointment")
+    this.appointment = saveappointment ? JSON.parse(saveappointment): []
+  } 
   title1: string = "";
   date1 : Date =  new Date();
   appointment : Appointment [] = [];
@@ -22,17 +26,17 @@ addAppointment() {
      this.appointment.push(newAppointment);
      this.title1 = "";
      this.date1 = new Date();
-
-
-     alert(this.appointment.length);
-
+     localStorage.setItem("appointment",JSON.stringify(this.appointment))
   }
+}
 
+delete(index : number)
+{
+this.appointment.splice(index,1);
+localStorage.setItem("appointment",JSON.stringify(this.appointment))
 
 
 }
-
-
 
 
 }
